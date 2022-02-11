3e4cffa25977472d9c30786c21c3e97cfc399557
Ebo Onwunali <ebo@njgeneral.com>
2022-02-11 12:21:28 -0500

    Finished code for this JavaScript exercise

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ console.log(
 );
 
 // Exercise 1
+const firstAlias = "Ihuoma";
+const  middleAlias = "Ebo";
+let lastAlias = "Onwunali";
+var age = 34;
 
 // Exercise 2
+let fullAlias = `${firstAlias} ${middleAlias} ${lastAlias}`;
+console.log(fullAlias);
+console.log("\n=================\n");
 
 // Exercise 3
+var city = "Los Angeles";
+var passTime = "learning";
+let simpleStory = `Hello, here is a simple story: my alias or what you may call a name is ${fullAlias}. 
+My residence is located in ${city}. And I enjoy ${passTime} and coding!`;
+console.log(simpleStory);
+
+console.log("\n=================\n");
